0288d533464fa341a5d0a27c64edd75233b4c4ff
Hongbin Bao <1450130140@qq.com>
2023-12-14 17:35:08 +0800

    fix:solve the cache penetration problem of shop queries 解决商铺查询的缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -39,12 +41,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.ok(shop);
         }
 
+        // 判断命中的是否是空值
 
+        if(shopJson != null){
+            // 返回一个错误信息
+            return Result.fail("店铺信息不存在！");
+
+        }
         // 4. 不存在，根据id查询数据库
         Shop shop = getById(id);
 
         // 5. 不存在 返回错误
         if (shop == null){
+            // 将空值写入redis
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
+            // 返回错误信息
             return Result.fail("店铺不存在！");
         }
         // 6. 存在 写入 redis
